1f17fa603118b3267cf89544307a43c1988b8d9d
shad khan <khan.shad65@gmail.com>
2022-12-28 05:37:01 +0530

    Temperature Converter

---
+++ src/main/java/com/mycompany/conversion/Conversion.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.conversion;
+
+/**
+ *
+ * @author Sohail
+ */
+public class Conversion {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
